3bbbff807e8f0207d79d94f87c805d03413d019f
Vere Miller <72162006+vmille25@users.noreply.github.com>
2021-05-27 14:42:39 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Captura;
+
+namespace screen_recorder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rec = new Recorder(new RecorderParams("out.avi", 10, SharpAvi.KnownFourCCs.Codecs.MotionJpeg, 70));
+            Console.WriteLine("Press any key to Stop...");
+            Console.ReadKey();
+
+            // Finish Writing
+            rec.Dispose();
+        }
+    }
+}
